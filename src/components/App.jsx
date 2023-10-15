de44513c88275ed9a511d9e45ff28ab7a7bd4b31
Maksim <75714743+Maksim8811@users.noreply.github.com>
2023-10-15 13:48:26 +0300

    problem logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,50 @@
-import { Routes, Route} from "react-router-dom"
-import { SharedLayout } from "./SharedLayout/SharedLayout"
+import { Routes, Route, Link} from "react-router-dom"
+// import { SharedLayout } from "./SharedLayout/SharedLayout"
 import {Main} from "./Main/Main"
 import {About} from "./About/About"
 import {Contacts} from "./Contacts/Contacts"
+import "../components/App.css"
 
 
 export const App = () => {
   return (
+
+    <div>
+        <header>
+          
+          
+        
+        {/* <button class="burger-btm" type="button">
+                <svg class="burger-icon">
+                    {/* <use xlink:href="./images/symbol-defs.svg#icon-logo_smile"></use> */}
+                    {/* <svg class="icon icon-logo_smile"><use xlink:href="#icon-logo_smile"></use></svg> */}
+                    {/* <use href="./images/symbol-defs.svg#icon-logo_smile"></use>
+                </svg>
+            </button> */} 
+
+
+<svg class="icon icon-logo_smile"><use href="./images/symbol-defs.svg#icon-logo_smile"></use></svg>
+        
+
+        
+        
+        
+        <nav>
+            <Link className="link" to="/">Головна</Link>
+            <Link to="/about">Про нас</Link>
+            <Link to="/contacts">Контакти</Link>
+        </nav>
+        
+        </header>
       
       <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Main />} />
+      <Route path="/" element={<Main />}/>
+        
         <Route path="about" element={<About />}/>
         <Route path="contacts" element={<Contacts />} />
-      </Route>
+      
       <Route path="*" element={<Main/>} />
     </Routes>
+    </div>
   )
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,19 @@
-import { Link, Outlet } from "react-router-dom"
+// import { Link, Outlet } from "react-router-dom"
 
-export const SharedLayout = () => {
-    return (
-        <div>
-        <header>
-        <nav>
-            <Link to="/">Головна</Link>
-            <Link to="/about">Про нас</Link>
-            <Link to="/contacts">Контакти</Link>
-        </nav>
+// export const SharedLayout = () => {
+//     return (
+//         <div>
+//         <header>
+//         <nav>
+//             <Link to="/">Головна</Link>
+//             <Link to="/about">Про нас</Link>
+//             <Link to="/contacts">Контакти</Link>
+//         </nav>
         
-        </header>
+//         </header>
 
-        <Outlet/>
+//         <Outlet/>
 
-    </div>
-    )
-}
+//     </div>
+//     )
+// }
