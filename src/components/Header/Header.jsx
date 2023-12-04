8a2379d5427243d6268008b014a0dc7702694a03
Maksim <75714743+Maksim8811@users.noreply.github.com>
2023-12-04 23:01:06 +0200

    add gradient in headers mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,8 +24,8 @@ console.log(sprite)
             <li className="header_list_li">
                 <button className="header_list_leng" type="button">UA</button></li>
             <li className="header_list_li">
-                <button button type="button">
-                <svg>
+                <button className="header_list_burger" button type="button">
+                <svg className="icon-burger">
                 <use href={sprite + "#icon-burger"}></use>
                 </svg>
                     </button></li>
