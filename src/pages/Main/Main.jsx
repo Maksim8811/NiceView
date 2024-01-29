cd62143f4f497181749cc55e1c48d3df65e932e6
Maksim <75714743+Maksim8811@users.noreply.github.com>
2024-01-29 16:43:53 +0200

    add style for desktop

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,23 +1,27 @@
 import "./Main.css"
 
 export const Main = () => {
+
     return (
-      
+      <>
+ 
       <section  className="main">
+        <div className="main_container">
         <div className="main_modal">
           <h2 className="main_modal_tittle">ЖК “Nice view”</h2>
-          <p className="main_modal_text">При 100% оплаті  знижка 5% від банку. Пропозиція дійсна до 31.12.22 р.</p>
+          <p className="main_modal_text">При 100% оплаті  знижка 5% від банку.<br/>
+          Пропозиція дійсна до 31.12.22 р.</p>
           <button className="main_modal_btn" type="button">Дзвінок</button>
         </div>
+        </div>
+       
         <div className="main_gradient">
           <p className="main_gradient_text">м.Київ, просп. Вознесенський 148</p>
         </div>
-        
-        {/* <div className="main_polygon">|</div> */}
+        <div className="main_img"></div>
       </section>
-        
-      
-      
+  
+     </>
      
     )
 }
