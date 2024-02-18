0a75c21489ba23681a2e1c29716cba20ddcdf176
Maksim <75714743+Maksim8811@users.noreply.github.com>
2024-02-18 22:23:06 +0200

    start styled section specific

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,40 +4,6 @@ import {Specific} from "../../components/Specific/Specific"
 
 export const Main = () => {
 
-  // Налаштування
-const recordCollection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ""
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
-
-// Змініть код лише під цим рядком
-function updateRecords(records, id, prop, value) {
-   if (records[id][prop] === value) {
-    
-   }
-  
-
-}
-
-// updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-// updateRecords(recordCollection, 1245, "tracks", "Addicted to Love");
-updateRecords(recordCollection, 2548, "tracks", "")
-
     return (
       <>
       <main>
