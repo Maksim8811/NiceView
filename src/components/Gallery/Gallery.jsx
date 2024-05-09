0ca7087c06fd9f1178c531064d121fb0b1d3d086
Maksim <75714743+Maksim8811@users.noreply.github.com>
2024-05-09 13:52:16 +0300

    add arrow for galerry

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,39 @@
 import "./Gallery.css"
+import { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import im1x from "../images/gallery_img/galley@1x.jpg"
+import im1x1 from "../images/gallery_img/galley@1x1.jpg"
+import im1x2 from "../images/gallery_img/galley@1x2.jpg"
+import im1x3 from "../images/gallery_img/galley@1x3.jpg"
+import im1x4 from "../images/gallery_img/galley@1x4.jpg"
+import im1x5 from "../images/gallery_img/galley@1x5.jpg"
 
-export const Gallery = () => {
+const galleryArr = [im1x, im1x1, im1x2, im1x3, im1x4, im1x5]
+
+export const Gallery = ({
+    infinity = true}) => {
+
+    const [currentImage, setCurrentImage] = useState(0)
+
+    const clickLeftArrow = () => {
+        if(infinity && currentImage === 0){
+            setCurrentImage(galleryArr.length - 1)
+        }
+        else {
+            setCurrentImage(currentImage - 1)
+        }
+        
+    }
+
+    const clickRightArrow = () => {
+        if(infinity && currentImage === galleryArr.length - 1) {
+            setCurrentImage(0)
+        }
+        else {
+            setCurrentImage(currentImage + 1)
+        }
+    }
 
     return (
         <>
@@ -11,8 +44,22 @@ export const Gallery = () => {
             </div>
 
             <div className="gallery_lib">
-                <p>del 16px radius</p>
-                {/* <img src={null}/> */}
+
+                {(infinity || currentImage > 0) && (
+                        <div className="left_arrow" onClick={clickLeftArrow}>
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                        </div>
+                )}
+
+                <img className="gallery_img" src={galleryArr[currentImage]} alt="apartment plan"/>
+
+                {(infinity || currentImage !== galleryArr.length - 1) && (
+                         <div className="right_arrow" onClick={clickRightArrow}>
+                         <FontAwesomeIcon icon={faChevronRight} />
+                         </div>
+         
+                )}
+               
             </div>
         </section>
         
