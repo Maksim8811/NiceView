c188ef681f4aa20c857d02dd2da5a36f610d6d5f
Maksim <75714743+Maksim8811@users.noreply.github.com>
2024-06-02 15:22:13 +0300

    add form

--- src/components/Consyltation/Consyltation.jsx
+++ src/components/Consyltation/Consyltation.jsx
@@ -10,6 +10,37 @@ export const Consyltation = () => {
             <div class="icon-Line-infrastructure_desktop"></div> 
         </div>
         <p className="consyltation_text">Поля відмічені  *  є обов’язковими для заповнення</p> 
+
+        <form>
+        <label>
+        ПІБ*
+          <input
+            type="text"
+            placeholder="Сергій Анатолійович"
+            
+          />
+        </label>
+
+        <label>
+        Номер телефону*
+          <input
+            type="text"
+            placeholder="+38 (000) 000 00 00"
+            
+          />
+        </label>
+
+        <label>
+        Зручний час
+          <input
+            type="text"
+            placeholder="00:00"
+            
+          />
+        </label>
+
+        <button type="submit">Дзвінок</button>
+      </form>
         </section>
         </>
     )
