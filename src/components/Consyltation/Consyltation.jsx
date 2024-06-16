8f794505cfd624452e3057d8f59d1344afecf9f8
Maksim <75714743+Maksim8811@users.noreply.github.com>
2024-06-16 18:56:59 +0300

    add styled consyltation section tablet

--- src/components/Consyltation/Consyltation.jsx
+++ src/components/Consyltation/Consyltation.jsx
@@ -1,16 +1,28 @@
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { useId } from "react"
 import "../../components/Consyltation/Consyltation.css"
+import * as Yup from "yup"
 
 export const Consyltation = () => {
+
+  const FeedbackSchema = Yup.object().shape({
+    username: Yup.string().min(2, "Мало символів!").max(50, "Багато символів!").required("Required"),
+    phone: Yup.string().min(10, "Мало символів!").max(19, "Багато символів!").required("Required"),
+    time: Yup.string().required("Required"),
+  });
 
   const userNameFieldId = useId()
   const phoneFieldId = useId()
   const timeFieldId = useId()
 
   const handleSubmit = (values, actions) => {
-		console.log(values);
-		actions.resetForm();
+    if(values.phone === "+38 (000) 000 00 00") {
+      alert("Введіть правильний номер")
+    } else {
+      alert(`Вам ${values.username} на номер ${values.phone} зателефонують о ${values.time}`)
+    }
+		
+		actions.resetForm()
 	};
 
     return (
@@ -23,61 +35,28 @@ export const Consyltation = () => {
         </div>
         <p className="consyltation_text">Поля відмічені  *  є обов’язковими для заповнення</p> 
 
-        {/* <form className="consyltation_form" onSubmit={handleSubmit}>
-        <label className="form_label" htmlFor={nameId}>
-        ПІБ*
-          <input className="label_input"
-            type="text"
-            name="name"
-            placeholder="Сергій Анатолійович"
-            id={nameId}
-            
-          />
-        </label>
-
-        <label className="form_label" htmlFor={numberId}>
-        Номер телефону*
-          <input className="label_input"
-            type="number"
-            name="number"
-            placeholder="+38 (000) 000 00 00"
-            id={numberId}
-            
-          />
-        </label>
-
-        <label className="form_label" htmlFor={timeId}>
-        Зручний час
-          <input className="label_input"
-            type="time"
-            name="time"
-            placeholder="00:00"
-            id={timeId}
-            
-          />
-        </label>
-        
-        <div className="consyltation_contbtn">
-           <button className="consyltation_btn" type="submit">Дзвінок</button>
-        </div>
-        
-      </form> */}
-
 <Formik initialValues={{
         username: "Сергій Анатолійович",
         phone: "+38 (000) 000 00 00",
         time: "00:00"
+}}
 
-}} 
- onSubmit={handleSubmit}>
+
+ onSubmit={handleSubmit}
+ 
+ validationSchema={FeedbackSchema}>
       <Form className="consyltation_form">
       <label className="form_label" htmlFor={userNameFieldId}>ПІБ*</label>
       <Field className="label_input" type="text" name="username" id={userNameFieldId}/>
+      <ErrorMessage className="error_massage" name="username" component="span" />
       <label className="form_label" htmlFor={phoneFieldId}> Номер телефону*</label>
 			<Field className="label_input" type="phone" name="phone" id={phoneFieldId}/>
+      <ErrorMessage className="error_massage" name="phone" component="span" />
       <label className="form_label" htmlFor={timeFieldId}>Зручний час</label>
       <Field className="label_input" type="time" name="time" id={timeFieldId}/>
-      {/* <button className="consyltation_btn" type="submit">Дзвінок</button> */}
+      <div className="consyltation_contbtn">
+           <button className="consyltation_btn" type="submit">Дзвінок</button>
+        </div>
 			</Form>
     </Formik>
 
